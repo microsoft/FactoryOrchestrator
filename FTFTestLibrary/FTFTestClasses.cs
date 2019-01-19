57c20767a6cf7576670c6e36e190338eefa66e45
Jake Friedman <jafriedm@microsoft.com>
2019-01-18 16:42:57 -0800

    Initial submit

---
+++ FTFTestLibrary/FTFTestClasses.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FTFTestLibrary
+{
+    public class FactoryTest
+    {
+        public FactoryTest()
+        {
+            IsTAEF = false;
+        }
+
+        // protected instance variables
+        public DateTime LastTimeRun { get; set; }
+        public bool? Result { get; set; }
+        protected String LogFilePath { get; set; }
+        public String TestPath;
+        public String TestName;
+        public bool IsTAEF { get; set; }
+        public List<String> Arguments;
+    }
+
+    public class TAEFTest : FactoryTest
+    {
+        public TAEFTest()
+        {
+            IsTAEF = true;
+        }
+
+        private List<TAEFTestCase> _testCases;
+        private String _wtlFilePath;
+    }
+    public class TAEFTestCase
+    {
+        public TAEFTestCase()
+        {
+        }
+
+        private String _name;
+        private bool? _result;
+    }
+
+    public class TestList
+    {
+        public List<FactoryTest> Tests;
+
+        private bool? _result;
+    }
+}
