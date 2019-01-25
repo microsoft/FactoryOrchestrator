6389d6171c2733367a69b301f86b881eb047db85
Jake Friedman <jafriedm@microsoft.com>
2019-01-24 17:36:09 -0800

    Add NETCore service, continue to improve Test classes

---
+++ FTFService/LogFileProvider.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FTFService
+{
+    public class LogFileProvider : ILoggerProvider
+    {
+        // Log to file next to the service binary
+        private static readonly String _logPath= Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "FTFService.log");
+        private static StreamWriter _logStream = null;
+        private static uint _logCount = 0;
+        private static object _logLock = new object();
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            // Synchronize to prevent multiple log streams from being created
+            lock (_logLock)
+            {
+                _logCount++;
+                if (_logStream == null)
+                {
+                    _logStream = new StreamWriter(_logPath);
+                }
+            }
+            return new FileLogger(this, categoryName);
+        }
+
+        public void Dispose()
+        {
+            lock (_logLock)
+            {
+                _logCount--;
+                if (_logCount == 0)
+                {
+                    _logStream.Close();
+                    _logStream.Dispose();
+                    _logStream = null;
+                }
+            }
+        }
+
+        public void AddMessage(DateTimeOffset timestamp, string message)
+        {
+            // Synchronize to ensure messages are printed in order
+            lock (_logLock)
+            {
+                _logStream.WriteLine(message);
+               // _logStream.Flush();
+            }
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        LogFileProvider _provider;
+        string _category;
+        public FileLogger(LogFileProvider provider, string categoryName)
+        {
+            _provider = provider;
+            _category = categoryName;
+        }
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            if (logLevel == LogLevel.None)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Log<TState>(DateTimeOffset timestamp, LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+
+            var builder = new StringBuilder();
+            builder.Append(timestamp.ToString("yyyy-MM-dd HH:mm:ss.fff zzz"));
+            builder.Append(" [");
+            builder.Append(logLevel.ToString());
+            builder.Append("] ");
+            builder.Append(_category);
+            builder.Append(": ");
+            builder.AppendLine(formatter(state, exception));
+
+            if (exception != null)
+            {
+                builder.AppendLine(exception.ToString());
+            }
+
+            _provider.AddMessage(timestamp, builder.ToString());
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            Log(DateTimeOffset.Now, logLevel, eventId, state, exception, formatter);
+        }
+    }
+}
--- FTFTestLibrary/FTFTestClasses.cs
+++ FTFTestLibrary/FTFTestClasses.cs
@@ -1,28 +1,139 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
-namespace FTFTestLibrary
+namespace FTFTestExecution
 {
+    public enum TestStatus
+    {
+        TestPassed,
+        TestFailed,
+        TAEFTestCasePassed,
+        TAEFTestCaseFailed,
+        TestWarning,
+        TestError,
+        TestException,
+        TestAborted,
+        TestRunning,
+        TestNotRun
+    }
+
     public class FactoryTest
     {
-        public FactoryTest()
+        public FactoryTest(String testPath)
         {
+            if (!File.Exists(testPath))
+            {
+                throw new FileNotFoundException(String.Format("Unable to find test binary at {0}", testPath));
+            }
+
             IsTAEF = false;
+            TestPath = testPath;
+            Reset();
+        }
+
+        public override string ToString()
+        {
+            return TestName;
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    try
+        //    {
+        //        if (String.Compare(((FactoryTest)obj).TestPath, TestPath, true) == 0)
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    catch (InvalidCastException)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        public void Reset()
+        {
+            LastTimeRun = null;
+            TestStatus = TestStatus.TestNotRun;
+            TestRunner = null;
+            ExitCode = null;
+            if (LogFilePath != null)
+            {
+                if (File.Exists(LogFilePath))
+                {
+                    try
+                    {
+                        File.Delete(LogFilePath);
+                    }
+                    catch { }
+                }
+            }
         }
 
         // protected instance variables
-        public DateTime LastTimeRun { get; set; }
-        public bool? Result { get; set; }
-        protected String LogFilePath { get; set; }
+        public DateTime? LastTimeRun { get; set; }
+        public TestStatus TestStatus { get; set; }
+
+        public bool? TestPassed
+        {
+            get
+            {
+                if (this.TestStatus == TestStatus.TestPassed)
+                {
+                    return true;
+                }
+                else if (this.TestStatus == TestStatus.TestFailed)
+                {
+                    return false;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+        }
+
+        public TimeSpan TestRunTime
+        {
+            get
+            {
+                if (TestRunner != null)
+                {
+                    return TestRunner._timer.Elapsed;
+                }
+                else
+                {
+                    return new TimeSpan();
+                }
+            }
+        }
+
+        public int? ExitCode { get; set; }
+        public String LogFilePath { get; set; }
         public String TestPath;
-        public String TestName;
+        public String TestName
+        {
+            get
+            {
+                return Path.GetFileName(TestPath);
+            }
+        }
         public bool IsTAEF { get; set; }
         public List<String> Arguments;
+
+        public TestRunner TestRunner;
     }
 
     public class TAEFTest : FactoryTest
     {
-        public TAEFTest()
+        public TAEFTest(string testPath) : base(testPath)
         {
             IsTAEF = true;
         }
@@ -36,14 +147,37 @@ namespace FTFTestLibrary
         {
         }
 
-        private String _name;
-        private bool? _result;
+        public String Name;
+        public TestStatus TestStatus { get; set; }
+        public bool? TestPassed
+        {
+            get
+            {
+                if (this.TestStatus == TestStatus.TAEFTestCasePassed)
+                {
+                    return true;
+                }
+                else if (this.TestStatus == TestStatus.TAEFTestCaseFailed)
+                {
+                    return false;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+        }
     }
 
-    public class TestList
+    public class TestList : IEnumerable
     {
         public List<FactoryTest> Tests;
-
         private bool? _result;
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)Tests).GetEnumerator();
+        }
     }
 }
