106a07fafc8874116168e7606f12afd275272c58
Molly Reilly <moreill@microsoft.com>
2019-02-19 09:44:32 -0800

    UI update: can select a testlist, tests dynamically appear, working on run buttons and results view

--- FTFUWP/MainPage.xaml.cs
+++ FTFUWP/MainPage.xaml.cs
@@ -16,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FTFTestExecution;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,6 +29,11 @@ namespace FTFUWP
     public sealed partial class MainPage : Page
     {
         IpcServiceClient<IFTFCommunication> client;
+        private Dictionary<Guid, TestList> testListMap = new Dictionary<Guid, TestList>();
+        public ObservableCollection<Guid> TestListGuids { get { return new ObservableCollection<Guid>(testListMap.Keys); } }
+
+        private ObservableCollection<Guid> testGuids = new ObservableCollection<Guid>();
+        public ObservableCollection<Guid> TestGuids { get { return testGuids; } }
 
         public MainPage()
         {
@@ -35,27 +42,63 @@ namespace FTFUWP
             client = new IpcServiceClientBuilder<IFTFCommunication>()
                 .UseTcp(IPAddress.Loopback, 45684)
                 .Build();
+            // We generate 10 TestLists each with 100 Tests, every 5 tests pass and the rest fail
+            GetTestListMap();
         }
 
-        private async void Button_PointerPressed(object sender, PointerRoutedEventArgs e)
+        private TestList getTestListAsync()
         {
-            this.result.Visibility = Visibility.Collapsed;
-            bool result = false;
-            if (client != null)
+            //var tests = await client.InvokeAsync(x => x.CreateTestListFromDirectory("c:\\data\\tests\\", false));
+            //result = await client.InvokeAsync(x => x.Run(tests.Guid, false, false));
+
+            // Test code to format UI
+            TestList t = new TestList(Guid.NewGuid());
+            for (int i = 0; i < 100; i++)
             {
-                try
+                TAEFTest g = new TAEFTest(i + "foo.dll")
                 {
-                    var tests = await client.InvokeAsync(x => x.CreateTestListFromDirectory("c:\\data\\tests\\", false));
-                    result = await client.InvokeAsync(x => x.Run(tests.Guid, false, false));
+                    LastTimeRun = DateTime.Now,
+                    ExitCode = 1
+                };
+                if (i % 5 == 0)
+                {
+                    g.TestStatus = TestStatus.TestPassed;
                 }
-                catch (Exception ex)
+                else
                 {
-                    ex.ToString();
+                    g.TestStatus = TestStatus.TestFailed;
+                }
+                t.Tests.Add(g.Guid, new Tuple<ExecutableTest, bool>(g, true));
+            }
+            return t;
+        }
+
+        private Dictionary<Guid, TestList> GetTestListMap()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                TestList t = getTestListAsync();
+                testListMap.Add(t.Guid, t);
+            }
+            return testListMap;
+        }
+
+        private ObservableCollection<Guid> GetTestGuids(Guid guid)
+        {
+            return new ObservableCollection<Guid>(testListMap[guid].Tests.Keys);
+        }
+
+        private void TestListsView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TestListsView.SelectedItem != null)
+            {
+                Guid testListGuid = (Guid)TestListsView.SelectedItem;
+                testGuids = GetTestGuids(testListGuid);
+                TestsView.Items.Clear();
+                foreach (Guid g in TestGuids) {
+                    TestsView.Items.Add(g);
                 }
             }
-
-            this.result.IsChecked = result;
-            this.result.Visibility = Visibility.Visible;
         }
     }
 }
