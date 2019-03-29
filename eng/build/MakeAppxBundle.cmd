3359612738e4f84c1b37be5d8125fe744f032ed9
Jake Friedman <jake.friedman@microsoft.com>
2019-03-29 21:13:16 +0000

    Merged PR 3082607: vpack build is close to complete
    - Debug and release have unique jobs
    - VPack created for release build
    - Appx is store signed
    
    Related work items: #19965135

--- eng/build/MakeAppxBundle.cmd
+++ eng/build/MakeAppxBundle.cmd
@@ -1,3 +1,7 @@
 echo.
-echo Creating Ship Multi-architecture(x86, x64, arm) Appx bundles ...
+echo Creating multi-architecture(x86, x64, arm) Appx bundles ...
+call %TFS_ToolsDirectory%\bin\MakeAppxBundle.cmd -appxBundleFilename "Microsoft.FTFUWP_8wekyb3d8bbwe.appxbundle" -projectDir "FTFUWP" -multi $True -signBundle $False -platforms "x86","x64","arm" -configurations "Debug"
+REM Comment out me for fast iteration
 call %TFS_ToolsDirectory%\bin\MakeAppxBundle.cmd -appxBundleFilename "Microsoft.FTFUWP_8wekyb3d8bbwe.appxbundle" -projectDir "FTFUWP" -multi $True -signBundle $True -signCert 136020001 -platforms "x86","x64","arm" -configurations "Release"
+REM Uncomment me for fast iteration
+REM call %TFS_ToolsDirectory%\bin\MakeAppxBundle.cmd -appxBundleFilename "Microsoft.FTFUWP_8wekyb3d8bbwe.appxbundle" -projectDir "FTFUWP" -multi $True -signBundle $False -platforms "x86","x64","arm" -configurations "Release"
