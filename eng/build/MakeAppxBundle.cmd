841c6449fc940d682d1d75854eba9381afb04d85
Jake Friedman <jake.friedman@microsoft.com>
2019-03-29 06:41:08 +0000

    Merged PR 3079858: Ready for vpack build

---
+++ eng/build/MakeAppxBundle.cmd
@@ -0,0 +1,3 @@
+echo.
+echo Creating Ship Multi-architecture(x86, x64, arm) Appx bundles ...
+call %TFS_ToolsDirectory%\bin\MakeAppxBundle.cmd -appxBundleFilename "Microsoft.FTFUWP_8wekyb3d8bbwe.appxbundle" -projectDir "FTFUWP" -multi $True -signBundle $True -signCert 136020001 -platforms "x86","x64","arm" -configurations "Release"
