e466da703da9c373a67e885b91fc33fa321ac850
Jake Friedman <jake.friedman@microsoft.com>
2019-04-02 02:40:51 +0000

    Merged PR 3093592: Create WMXML for FTFService (per arch)
    Related work items: #19965135

---
+++ eng/build/CreateServiceManifest.ps1
@@ -0,0 +1,72 @@
+Param
+(
+    [Parameter(Mandatory = $true)][string]$PublishFolder,
+    [Parameter(Mandatory = $false)][string]$SourceRootFolder = "",
+	[Parameter(Mandatory = $true)][string]$OutputFile,
+    [Parameter(Mandatory = $true)][string]$Owner,
+    [Parameter(Mandatory = $true)][string]$Namespace,
+    [Parameter(Mandatory = $true)][string]$Name
+)
+
+$ErrorActionPreference = "stop"
+
+$DestinationRootDir = "`$(runtime.system32)\manufacturing\FTFService"
+
+[xml] $templateXml = Get-Content "$PSScriptRoot\FTFServiceTemplate.wm.xml"
+$wmxmlNs = "urn:Microsoft.CompPlat/ManifestSchema.v1.00"
+$ns = @{wmxml = "$wmxmlNs"}
+
+Write-Host "Creating manifest $OutputFile from $PublishFolder"
+
+# Set manifest info
+$rootNode = Select-Xml -Xml $templateXml -XPath "//wmxml:identity" -Namespace $ns | Select -First 1
+$rootNode.Node.SetAttribute("owner", "$Owner")
+$rootNode.Node.SetAttribute("namespace", "$Namespace")
+$rootNode.Node.SetAttribute("name", "$Name")
+
+$filesNode = Select-Xml -Xml $templateXml -XPath "//wmxml:files" -Namespace $ns | Select -First 1
+
+[System.IO.DirectoryInfo] $PublishDir = $PublishFolder
+$filesToAdd = Get-ChildItem $PublishFolder -Recurse -File
+
+foreach ($file in $filesToAdd)
+{
+    # Default destination
+    $fileDestinationDir = $DestinationRootDir
+    # If file was in a subfolder, append it
+    if (-not ($file.DirectoryName -like $PublishDir.FullName))
+    {
+        $subfolder = $file.DirectoryName.SubString($PublishDir.FullName.Length + 1)
+        $fileDestinationDir += "\$subfolder"
+    }
+    else
+    {
+        $subfolder = ""
+    }
+
+    # Source is relative to SourceRootFolder if given
+	if ($SourceRootFolder -ne "")
+	{
+		$source = $SourceRootFolder + "\" + $subfolder + "\" + $file.Name
+	}
+	else
+	{
+		$source = $file.FullName
+	}
+
+    # Clean extra slashes
+    $source = $source.Replace("\\\", "\\")
+    $source = $source.Replace("\\", "\")
+
+    $newElement = $templateXml.CreateElement("file", $wmxmlNs)
+    $newElement.SetAttribute("destinationDir", "$fileDestinationDir")
+    $newElement.SetAttribute("securityDescriptor", "WRP_FILE_DEFAULT_SDDL")
+    $newElement.SetAttribute("source", "$source")
+    $node = $filesNode.Node.AppendChild($newElement)
+	Write-Host "Added $($file.FullName)."
+	Write-Host "---source: $source"
+	Write-Host "---destinationDir: $fileDestinationDir"
+}
+
+Write-Host "Saving $OutputFile"
+$templateXml.Save($OutputFile)
---
+++ eng/build/CreateServiceManifestForPkgES.ps1
@@ -0,0 +1,12 @@
+Param
+(
+    [Parameter(Mandatory = $true)][string]$BuildConfiguration,
+    [Parameter(Mandatory = $true)][string]$NtTreeRootFolder,
+    [Parameter(Mandatory = $true)][string]$BinFolder
+)
+
+$ErrorActionPreference = "stop"
+& "$PSScriptRoot\CreateServiceManifest.ps1" -PublishFolder "$BinFolder\$BuildConfiguration\x86\FTFService_SCD" -SourceRootFolder "`$(build.nttree)\$NtTreeRootFolder\x86\FTFService_SCD" -OutputFile "$BinFolder\$BuildConfiguration\x86\FTFService_SCD\Microsoft-OneCore-FactoryTestFramework-Service-x86.wm.xml" -Owner "Microsoft" -Namespace "OneCore" -Name "FactoryTestFramework-Service-x86"
+& "$PSScriptRoot\CreateServiceManifest.ps1" -PublishFolder "$BinFolder\$BuildConfiguration\x64\FTFService_SCD" -SourceRootFolder "`$(build.nttree)\$NtTreeRootFolder\x64\FTFService_SCD" -OutputFile "$BinFolder\$BuildConfiguration\x64\FTFService_SCD\Microsoft-OneCore-FactoryTestFramework-Service-x64.wm.xml" -Owner "Microsoft" -Namespace "OneCore" -Name "FactoryTestFramework-Service-x64"
+& "$PSScriptRoot\CreateServiceManifest.ps1" -PublishFolder "$BinFolder\$BuildConfiguration\ARM\FTFService_SCD" -SourceRootFolder "`$(build.nttree)\$NtTreeRootFolder\ARM\FTFService_SCD" -OutputFile "$BinFolder\$BuildConfiguration\ARM\FTFService_SCD\Microsoft-OneCore-FactoryTestFramework-Service-ARM.wm.xml" -Owner "Microsoft" -Namespace "OneCore" -Name "FactoryTestFramework-Service-ARM"
+& "$PSScriptRoot\CreateServiceManifest.ps1" -PublishFolder "$BinFolder\$BuildConfiguration\ARM64\FTFService_SCD" -SourceRootFolder "`$(build.nttree)\$NtTreeRootFolder\ARM64\FTFService_SCD" -OutputFile "$BinFolder\$BuildConfiguration\ARM64\FTFService_SCD\Microsoft-OneCore-FactoryTestFramework-Service-ARM64.wm.xml" -Owner "Microsoft" -Namespace "OneCore" -Name "FactoryTestFramework-Service-ARM64"
