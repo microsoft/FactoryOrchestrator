59280748449f829ba190c8117ba97ebc5f68929e
Jake Friedman <jake.friedman@microsoft.com>
2019-10-07 21:24:11 +0000

    Merged PR 3839659: Create vPacks for FO Client Sample and Client Libraries
    Related work items: #23608295, #23614814

--- ClientSample/Program.cs
+++ ClientSample/Program.cs
@@ -10,9 +10,12 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using TaskStatus = Microsoft.FactoryOrchestrator.Core.TaskStatus;
 
-namespace NETCoreClientSample
+namespace FactoryOrchestratorClientSample
 {
-    class Program
+    /// <summary>
+    /// 
+    /// </summary>
+    class FactoryOrchestratorNETCoreClientSample
     {
         static void Main(string[] args)
         {
@@ -20,12 +23,21 @@ namespace NETCoreClientSample
             t.Wait();
         }
 
+        /// <summary>
+        /// Main method for Client Sample. Factory Orchestrator service methods are all async.
+        /// 1) Connect to DUT
+        /// 2) Copy files and FactoryOrchestratorXML to DUT
+        /// 3) Load TaskLists from FactoryOrchestratorXML
+        /// 4) Execute loaded TaskLists
+        /// 5) Print results
+        /// 6) Copy logs to host PC
+        /// </summary>
         private static async Task RunAsync(string[] args)
         {
             TimeStarted = DateTime.Now;
 
             ValidateArgs(args);
-            await ConnectToFactoryOrchestrator(Ip, 45684);
+            await ConnectToFactoryOrchestrator(Ip);
             var FOXMLs = await CopyFilesToDUT(TestDir, DestDir);
             var taskListSummaries = await LoadFactoryOrchestratorXMLs(DestDir, FOXMLs);
             await ExecuteTaskLists(taskListSummaries);
@@ -33,6 +45,9 @@ namespace NETCoreClientSample
             await CopyLogsFromDUT(LogFolder);
         }
 
+        /// <summary>
+        /// Verifies arguments to program are valid.
+        /// </summary>
         private static void ValidateArgs(string[] args)
         {
             try
@@ -62,6 +77,9 @@ namespace NETCoreClientSample
             }
         }
 
+        /// <summary>
+        /// Prints usage if arguments were invalid.
+        /// </summary>
         private static void PrintUsage(ArgumentException e)
         {
             Console.WriteLine();
@@ -71,14 +89,25 @@ namespace NETCoreClientSample
             throw e;
         }
 
-        private static async Task ConnectToFactoryOrchestrator(IPAddress ip, int port)
+        /// <summary>
+        /// Instantiates a FactoryOrchestratorClient object to communicate with the Service.
+        /// </summary>
+        /// <param name="ip">Ip to connect to</param>
+        private static async Task ConnectToFactoryOrchestrator(IPAddress ip)
         {
-            Client = new FactoryOrchestratorClient(ip, port);
+            // Factory Orchestrator service uses port 45684
+            Client = new FactoryOrchestratorClient(ip, 45684);
             await Client.Connect();
 
             Console.WriteLine($"Connected to {ip.ToString()}");
         }
 
+        /// <summary>
+        /// Copies all files in target folder to the DUT.
+        /// </summary>
+        /// <param name="testDir">Target folder</param>
+        /// <param name="destDir">Destination folder</param>
+        /// <returns>Filename of any XML files found.</returns>
         private static async Task<List<string>> CopyFilesToDUT(string testDir, string destDir)
         {
             Console.WriteLine($"Copying latest binaries and TaskLists from {testDir} to {destDir} on device...");
@@ -87,6 +116,11 @@ namespace NETCoreClientSample
             return Directory.EnumerateFiles(testDir, "*.xml").ToList();
         }
 
+        /// <summary>
+        /// Imports any FactoryOrchestratorXML files found on a target folder on the DUT.
+        /// </summary>
+        /// <param name="destDir">Target folder</param>
+        /// <param name="FOXMLs">Filenames of XML files to import</param>
         private static async Task<List<TaskListSummary>> LoadFactoryOrchestratorXMLs(string destDir, List<string> FOXMLs)
         {
             Console.WriteLine("Loading TaskList(s) from FactoryOrchestratorXML file(s)...");
@@ -98,6 +132,7 @@ namespace NETCoreClientSample
 
             Console.WriteLine($"{Environment.NewLine}TaskLists:");
 
+            // Sort loaded TaskLists by name
             var tasklistSummaries = (await Client.GetTaskListSummaries()).OrderBy(x => x.Name);
             foreach (var summary in tasklistSummaries)
             {
@@ -107,6 +142,10 @@ namespace NETCoreClientSample
             return tasklistSummaries.ToList();
         }
 
+        /// <summary>
+        /// Runs TaskLists, printing out results.
+        /// </summary>
+        /// <param name="tasklistSummaries">List of TaskListSummary objects returned by GetTaskListSummaries Method</param>
         private static async Task ExecuteTaskLists(List<TaskListSummary> tasklistSummaries)
         {
             foreach (var summary in tasklistSummaries)
@@ -140,6 +179,9 @@ namespace NETCoreClientSample
             }
         }
 
+        /// <summary>
+        /// Prints out the final result of all the executed Tasks and TaskLists.
+        /// </summary>
         private static async Task PrintFinalResult()
         {
             var tasklistSummaries = (await Client.GetTaskListSummaries()).OrderBy(x => x.Name);
@@ -168,6 +210,10 @@ namespace NETCoreClientSample
             }
         }
 
+        /// <summary>
+        /// Copies all Factory Orchestrator logs to the local PC.
+        /// </summary>
+        /// <param name="destinationPath">Target folder on local PC.</param>
         private static async Task CopyLogsFromDUT(string destinationPath)
         {
             var logDir = await Client.GetLogFolder();
---
+++ eng/build/UpdateClientSampleForWSK.ps1
@@ -0,0 +1,40 @@
+Param
+(
+    [string]$ProjectFolder
+)
+
+[string]$outputContent = ""
+Write-Host "Modifying csproj in $ProjectFolder for WSK use..."
+$projs = Get-ChildItem -Path "$ProjectFolder" -Filter "*.csproj"
+foreach ($proj in $projs)
+{
+    $content = Get-Content $proj.FullName
+    $added = $false
+    foreach ($line in $content)
+    {
+        if (-not ($line -like "*ProjectReference*Include*"))
+        {
+            $outputContent += $line + "`n"
+        }
+        elseif ($added -eq $false)
+        {
+            # Add all the needed references, but for the WSK
+            $outputContent += "    <Reference Include=`"FactoryOrchestratorClientLibrary`">`n"
+            $outputContent += "      <HintPath>`$(WSKWorkspaceRoot)\Testing\Development\lib\NETStandard\FactoryOrchestratorClientLibrary.dll</HintPath>`n"
+            $outputContent += "    </Reference>`n"
+            $outputContent += "    <Reference Include=`"FactoryOrchestratorCoreLibrary`">`n"
+            $outputContent += "      <HintPath>`$(WSKWorkspaceRoot)\Testing\Development\lib\NETStandard\FactoryOrchestratorCoreLibrary.dll</HintPath>`n"
+            $outputContent += "    </Reference>`n"
+            $outputContent += "    <Reference Include=`"IpcServiceFramework.Client`">`n"
+            $outputContent += "      <HintPath>`$(WSKWorkspaceRoot)\Testing\Development\lib\NETStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Client.dll</HintPath>`n"
+            $outputContent += "    </Reference>`n"
+            $outputContent += "    <Reference Include=`"IpcServiceFramework.Core`">`n"
+            $outputContent += "      <HintPath>`$(WSKWorkspaceRoot)\Testing\Development\lib\NETStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Core.dll</HintPath>`n"
+            $outputContent += "    </Reference>`n"
+
+            $added = $true
+        }
+    }
+
+    Set-Content -Path "$($proj.FullName)" -Value $outputContent
+}
