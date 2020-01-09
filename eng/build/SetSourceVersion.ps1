88a9f9e22915ff65fdc303bd3b158915feec18d6
Jake Friedman <jake.friedman@microsoft.com>
2020-01-09 09:56:28 +0000

    Merged PR 4183997: Version FO to 1.0
    Version FO to 1.0
    
    Related work items: #23577204

--- eng/build/SetSourceVersion.ps1
+++ eng/build/SetSourceVersion.ps1
@@ -3,8 +3,18 @@ Param
 [string]$SrcPath
 )
 
+$ErrorActionPreference = "stop"
+
 $buildNumber = $env:TFS_VersionNumber
 $tfs = $false
+
+[xml]$customprops = Get-Content "$PSScriptRoot\..\..\custom.props"
+$msbldns = "http://schemas.microsoft.com/developer/msbuild/2003"
+$ns = @{msbld = "$msbldns"}
+$majorNode = Select-Xml -Xml $customprops -XPath "//msbld:VersionMajor" -Namespace $ns | Select -First 1
+$minorNode = Select-Xml -Xml $customprops -XPath "//msbld:VersionMinor" -Namespace $ns | Select -First 1
+$majorVersion = $majorNode.Node."#text"
+$minorVersion = $minorNode.Node."#text"
 
 if ($buildNumber -eq $null)
 {
@@ -15,28 +25,26 @@ if ($buildNumber -eq $null)
         $month = "0" + $month
     }
 
-    $buildNumber = "0.0." + $date.Year.ToString().SubString(2) + "" + $month + "." + $date.Hour.ToString() + $date.Minute.ToString()
+    $buildNumber = "$majorVersion.$minorVersion." + $date.Year.ToString().SubString(2) + "" + $month + "." + $date.Hour.ToString() + $date.Minute.ToString()
 }
 else
 {
     $tfs = $true
-    $buildNumber = "1.0." + $buildNumber
 }
 
 $file = Get-Item -Path "$SrcPath\Properties\AssemblyInfo.cs"
-Write-Host "Creating assembly info file for:" $file.FullName
 $tempFile = $env:TEMP + "\" + $file.Name + ".tmp"
-    
+
 #now load all content of the original file and rewrite modified to the same file
-if ($tfs -eq $true)
+ if ($tfs -eq $true)
 {
     Get-Content $file.FullName |
-    %{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyVersion(""$buildNumber"")" } |
-    %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyFileVersion(""$buildNumber"")" } |
     %{$_ -replace 'AssemblyDescription.+', "AssemblyDescription("""")]" }  > $tempFile
 }
 else
 {
+    Write-Host "Creating assembly info file for:" $file.FullName " build number:" $buildNumber
+
     Get-Content $file.FullName |
     %{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyVersion(""$buildNumber"")" } |
     %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyFileVersion(""$buildNumber"")" } |
