f9ce2db0f4e8a14e87794f28a915a2aee4d923d8
Jake Friedman <jake.friedman@microsoft.com>
2020-01-09 08:20:19 +0000

    Merged PR 4183806: New WSK sample lib location
    New WSK sample lib location
    
    Related work items: #23917313

--- eng/build/UpdateClientSampleForWSK.ps1
+++ eng/build/UpdateClientSampleForWSK.ps1
@@ -20,16 +20,16 @@ foreach ($proj in $projs)
         {
             # Add all the needed references, but for the WSK
             $outputContent += "    <Reference Include=`"FactoryOrchestratorClientLibrary`">`n"
-            $outputContent += "      <HintPath>`$(WSKContentRoot)\Testing\Development\lib\NETStandard\FactoryOrchestratorClientLibrary.dll</HintPath>`n"
+            $outputContent += "      <HintPath>`..\lib\NetStandard\FactoryOrchestratorClientLibrary.dll</HintPath>`n"
             $outputContent += "    </Reference>`n"
             $outputContent += "    <Reference Include=`"FactoryOrchestratorCoreLibrary`">`n"
-            $outputContent += "      <HintPath>`$(WSKContentRoot)\Testing\Development\lib\NETStandard\FactoryOrchestratorCoreLibrary.dll</HintPath>`n"
+            $outputContent += "      <HintPath>`..\lib\NetStandard\FactoryOrchestratorCoreLibrary.dll</HintPath>`n"
             $outputContent += "    </Reference>`n"
             $outputContent += "    <Reference Include=`"IpcServiceFramework.Client`">`n"
-            $outputContent += "      <HintPath>`$(WSKContentRoot)\Testing\Development\lib\NETStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Client.dll</HintPath>`n"
+            $outputContent += "      <HintPath>`..\lib\NetStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Client.dll</HintPath>`n"
             $outputContent += "    </Reference>`n"
             $outputContent += "    <Reference Include=`"IpcServiceFramework.Core`">`n"
-            $outputContent += "      <HintPath>`$(WSKContentRoot)\Testing\Development\lib\NETStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Core.dll</HintPath>`n"
+            $outputContent += "      <HintPath>`..\lib\NetStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Core.dll</HintPath>`n"
             $outputContent += "    </Reference>`n"
 
             $added = $true
