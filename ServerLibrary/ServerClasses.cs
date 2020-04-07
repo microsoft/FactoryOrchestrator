8a57da8974ca9d9efebd7f17e704ddc8150a073c
Ali Hajy <alhajy@microsoft.com>
2020-04-07 19:28:48 +0000

    Merged PR 4527673: remove the -NoProfile from the pwsh.exe call
    remove the -NoProfile from the pwsh.exe call

--- ServerLibrary/ServerClasses.cs
+++ ServerLibrary/ServerClasses.cs
@@ -1765,7 +1765,7 @@ namespace Microsoft.FactoryOrchestrator.Server
             else if (ActiveTaskRun.TaskType == TaskType.PowerShell)
             {
                 startInfo.FileName = "pwsh.exe";
-                startInfo.Arguments += $"-NonInteractive -NoProfile -File \"{ActiveTaskRun.TaskPath}\" ";
+                startInfo.Arguments += $"-NonInteractive -File \"{ActiveTaskRun.TaskPath}\" ";
             }
             else if (ActiveTaskRun.TaskType == TaskType.BatchFile)
             {
