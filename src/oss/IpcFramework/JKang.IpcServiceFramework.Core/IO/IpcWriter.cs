2200249523a7532d32c3c34d5e1cf8ca254d246e
Jake Friedman <jafriedm@microsoft.com>
2020-09-10 12:46:01 -0700

    Add latest IPC code from develop branch

---
+++ src/oss/IpcFramework/JKang.IpcServiceFramework.Core/IO/IpcWriter.cs
@@ -0,0 +1,83 @@
+﻿using JKang.IpcServiceFramework.Services;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JKang.IpcServiceFramework.IO
+{
+    public class IpcWriter : IDisposable
+    {
+        private readonly byte[] _lengthBuffer = new byte[4];
+        private readonly Stream _stream;
+        private readonly IIpcMessageSerializer _serializer;
+        private readonly bool _leaveOpen;
+
+        public IpcWriter(Stream stream, IIpcMessageSerializer serializer)
+            : this(stream, serializer, leaveOpen: false)
+        { }
+
+        public IpcWriter(Stream stream, IIpcMessageSerializer serializer, bool leaveOpen)
+        {
+            _stream = stream;
+            _serializer = serializer;
+            _leaveOpen = leaveOpen;
+        }
+
+        public async Task WriteAsync(IpcRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            byte[] binary = _serializer.SerializeRequest(request);
+            await WriteMessageAsync(binary, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task WriteAsync(IpcResponse response,
+            CancellationToken cancellationToken = default)
+        {
+            byte[] binary = _serializer.SerializeResponse(response);
+            await WriteMessageAsync(binary, cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task WriteMessageAsync(byte[] binary, CancellationToken cancellationToken)
+        {
+            int length = binary.Length;
+            _lengthBuffer[0] = (byte)length;
+            _lengthBuffer[1] = (byte)(length >> 8);
+            _lengthBuffer[2] = (byte)(length >> 16);
+            _lengthBuffer[3] = (byte)(length >> 24);
+
+            await _stream.WriteAsync(_lengthBuffer, 0, _lengthBuffer.Length, cancellationToken).ConfigureAwait(false);
+            await _stream.WriteAsync(binary, 0, binary.Length, cancellationToken).ConfigureAwait(false);
+        }
+
+        #region IDisposible
+
+        bool _disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                if (!_leaveOpen)
+                {
+                    _stream.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+
+        #endregion
+    }
+}
---
+++ src/oss/IpcFramework/JKang.IpcServiceFramework.Core/Services/DefaultIpcMessageSerializer.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace JKang.IpcServiceFramework.Services
+{
+    public class DefaultIpcMessageSerializer : IIpcMessageSerializer
+    {
+        private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Objects
+        };
+
+        public IpcRequest DeserializeRequest(byte[] binary)
+        {
+            return Deserialize<IpcRequest>(binary);
+        }
+
+        public IpcResponse DeserializeResponse(byte[] binary)
+        {
+            return Deserialize<IpcResponse>(binary);
+        }
+
+        public byte[] SerializeRequest(IpcRequest request)
+        {
+            return Serialize(request);
+        }
+
+        public byte[] SerializeResponse(IpcResponse response)
+        {
+            return Serialize(response);
+        }
+
+        private T Deserialize<T>(byte[] binary)
+        {
+            try
+            {
+                string json = Encoding.UTF8.GetString(binary);
+                return JsonConvert.DeserializeObject<T>(json, _settings);
+            }
+            catch (Exception ex) when (
+                ex is JsonSerializationException ||
+                ex is ArgumentException ||
+                ex is EncoderFallbackException)
+            {
+                throw new IpcSerializationException("Failed to deserialize IPC message", ex);
+            }
+        }
+
+        private byte[] Serialize(object obj)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(obj, _settings);
+                return Encoding.UTF8.GetBytes(json);
+            }
+            catch (Exception ex) when (
+                ex is JsonSerializationException ||
+                ex is EncoderFallbackException)
+            {
+                throw new IpcSerializationException("Failed to serialize IPC message", ex);
+            }
+        }
+    }
+}
