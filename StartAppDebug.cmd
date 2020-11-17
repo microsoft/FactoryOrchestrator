200e8f600971aa61f19c28f79df9f38611e0bae2
Jake Friedman <jafriedm@microsoft.com>
2020-11-17 15:36:50 -0800

    remove more junk

--- StartAppDebug.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-start cmd.exe /C "explorer.exe shell:appsFolder\Microsoft.FactoryOrchestratorApp.DEV_8wekyb3d8bbwe!App"
--- StartServiceDebug.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-start cmd.exe /C "dotnet %~dp0\bin\Debug\AnyCPU\FactoryOrchestratorService\netcoreapp3.1\FactoryOrchestratorService.dll & PAUSE"
