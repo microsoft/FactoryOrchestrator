92190ea393cc71bec494214d471624e3e39089dd
Jake Friedman <jake.friedman@microsoft.com>
2021-01-09 04:35:16 +0000

    Updated CopyUniqueBins.ps1

--- build/internal/CopyUniqueBins.ps1
+++ build/internal/CopyUniqueBins.ps1
@@ -7,7 +7,7 @@ Param
 (
     [Parameter(ParameterSetName = 'Find')][string]$MappingFileOut,
     [Parameter(ParameterSetName = 'Find')][string]$SearchFilter = "*",
-    [Parameter(ParameterSetName = 'Find')][Parameter(ParameterSetName = 'CopyMapping')][string]$CopyDestination,
+    [Parameter(ParameterSetName = 'Find')],
     [Parameter(ParameterSetName = 'CopyMapping')][string]$MappingFileIn,
     [Parameter(ParameterSetName = 'Find')][Parameter(ParameterSetName = 'CopyMapping')][string]$CopySource
 )
