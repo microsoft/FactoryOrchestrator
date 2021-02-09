2ac3996188f92cd78cfa760cd54fe0ef93999055
Jake Friedman <31931010+spaceisfun@users.noreply.github.com>
2021-02-08 17:19:23 -0800

    Fix ResourceManager path (#88)
    fix for Bug 31261377: [RI-TP] Failed FactoryOrchestratorTest.FactoryOrchestratorTest.ExecuteNetCoreTest#ClientSampleIntegrationTests: Test Run Failed.

--- src/ClientSample/Resources/Resources.Designer.cs
+++ src/ClientSample/Resources/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Microsoft.FactoryOrchestrator.ClientSample {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ClientSample.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.FactoryOrchestrator.ClientSample.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
