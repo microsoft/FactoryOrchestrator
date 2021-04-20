7f6c3a15716d795c9e9ab7f7928ac6a56c96d18b
Jake Friedman <jafriedm@microsoft.com>
2021-04-19 18:28:01 -0700

    errorcode

--- build/internal/UpdateClientSampleForWSK.ps1
+++ build/internal/UpdateClientSampleForWSK.ps1
@@ -3,68 +3,14 @@
 
 Param
 (
-    [string]$ProjectFolder
+    [string]$RepositoryRoot
 )
 
-[string]$outputContent = ""
-Write-Host "Modifying csproj in $ProjectFolder for WSK use..."
-$projs = Get-ChildItem -Path "$ProjectFolder" -Filter "*.csproj"
-foreach ($proj in $projs)
+$ErrorActionPreference = "stop"
+Set-Location $RepositoryRoot
+
+if (!(Test-Path ".\src\FactoryOrchestrator.sln"))
 {
-    $content = Get-Content $proj.FullName
-    $added = $false
-    foreach ($line in $content)
-    {
-        if (($line -like "*<Target*BeforeBuildPS*") -or ($line -like "*<PropertyGroup*XES_OUTDIR*!=*"))
-        {
-            Write-host "skip $line"
-            $inskip = $true
-            continue
-        }
-        elseif ($line -like "*</Target>*")
-        {
-            Write-host "skip $line"
-            $inskip = $false
-            continue
-        }
-        elseif (($line -like "*</PropertyGroup>*") -and $inskip)
-        {
-            Write-host "skip  $line"
-            $inskip = $false
-            continue
-        }
-
-        if (-not $inskip)
-        {
-            if ($line -like "*<OutputPath>*")
-            {
-                # fix output path
-                $outputContent += '    <OutputPath>.\bin\$(Configuration)\$(Platform)\$(TargetName)</OutputPath>' + "`n"
-            }
-            elseif (-not ($line -like "*ProjectReference*Include*"))
-            {
-                $outputContent += $line + "`n"
-            }
-            elseif ($added -eq $false)
-            {
-                # Add all the needed references, but for the WSK
-                $outputContent += "    <Reference Include=`"Microsoft.FactoryOrchestrator.Client`">`n"
-                $outputContent += "      <HintPath>`..\lib\NetStandard\Microsoft.FactoryOrchestrator.Client.dll</HintPath>`n"
-                $outputContent += "    </Reference>`n"
-                $outputContent += "    <Reference Include=`"Microsoft.FactoryOrchestrator.Core`">`n"
-                $outputContent += "      <HintPath>`..\lib\NetStandard\Microsoft.FactoryOrchestrator.Core.dll</HintPath>`n"
-                $outputContent += "    </Reference>`n"
-                $outputContent += "    <Reference Include=`"IpcServiceFramework.Client`">`n"
-                $outputContent += "      <HintPath>`..\lib\NetStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Client.dll</HintPath>`n"
-                $outputContent += "    </Reference>`n"
-                $outputContent += "    <Reference Include=`"IpcServiceFramework.Core`">`n"
-                $outputContent += "      <HintPath>`..\lib\NetStandard\OpenSourceSoftware\IpcServiceFramework\IpcServiceFramework.Core.dll</HintPath>`n"
-                $outputContent += "    </Reference>`n"
-    
-                $added = $true
-            }
-        }
-    }
-
-    Set-Content -Path "$($proj.FullName)" -Value $outputContent
+    Write-Error "$RepositoryRoot is not the root of the Factory Orchestrator repository!"
 }
+
